8437a7a8fe6bdc29becfce86fc8a3e6c03e6a04c
MarioIvan44 <20210050@ricaldone.edu.sv>
2025-07-19 21:08:00 -0500

    TODO BIEN 😭

--- proyectoPTC/src/main/java/apiTrackline/proyectoPTC/Controllers/AduanaController/Aduana.java
+++ proyectoPTC/src/main/java/apiTrackline/proyectoPTC/Controllers/AduanaController/Aduana.java
@@ -1,10 +1,7 @@
 package apiTrackline.proyectoPTC.Controllers.AduanaController;
 
-import apiTrackline.proyectoPTC.Entities.AduanaEntity;
 import apiTrackline.proyectoPTC.Models.DTO.DTOAduana;
-import apiTrackline.proyectoPTC.Models.DTO.DTOTipoServicio;
 import apiTrackline.proyectoPTC.Services.AduanaService;
-import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
